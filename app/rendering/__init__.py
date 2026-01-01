75a24d9ff56248c1e6e90912a95cb734f0e0a581
navlisData <navlis.stuff@gmail.com>
2026-01-01 21:37:31 +0100

    chore: use debug renderer (#8)
    * chore: move debug-rendering into rendering package
    
    * chore: overloaded protocol functions

--- app/rendering/__init__.py
+++ app/rendering/__init__.py
@@ -2,5 +2,6 @@ from __future__ import annotations
 
 from .agent_renderer import draw_agents
 from .graph_renderer import draw_active_edges
+from .debug_renderer import draw_debug_overlays
 
-__all__ = ["draw_agents", "draw_active_edges"]
+__all__ = ["draw_agents", "draw_active_edges", "draw_debug_overlays"]
